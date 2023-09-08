3f3ba7321a30402c890c8b4b31175bf947d03727
Tanisha Raj <ravi561956@gmail.com>
2023-09-08 21:07:19 +0530

    javafreestyle page added.

---
+++ Car.java
@@ -0,0 +1,11 @@
+/**
+ * Car
+ */
+public class Car {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
